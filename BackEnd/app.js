70b485ba0c0c7e7c9bade49613a37bdaca82188c
RasoulJf <rasouljalilian8@gmail.com>
2025-03-14 19:54:22 +0330

    Update app.js

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("Public"));
+app.use(express.static("Public")); 
 app.use("*", (req, res, next) => {
   next(new HandleERROR("Route not Found", 404));
 });
