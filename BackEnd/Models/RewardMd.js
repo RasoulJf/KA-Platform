71824083c1193efe27ca78747313506330d0c102
RasoulJf <rasouljalilian8@gmail.com>
2025-03-16 12:17:52 +0330

    Update RewardMd.js

---
+++ BackEnd/Models/RewardMd.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const rewardSchema = new mongoose.Schema({
+  parent: {
+    type: String,
+    enum: ["پاداش های عمومی", "پاداش های اختصاصی", "پاداش نیکوکارانه"],
+    required: true,
+  },
+  name: {
+    type: String,
+    required: [true, "Activity name is required"],
+  },
+  description: {
+    type: String,
+  },
+  minToken: {
+    type: Number,
+  },
+  reqToken: {
+    type: Number,
+  },
+});
+
+const Reward = mongoose.model("Reward", rewardSchema);
+
+export default Reward;
