aab4cfb786116e023cff99a882120e2e2273419f
RasoulJf <rasouljalilian8@gmail.com>
2025-03-16 11:27:32 +0330

    Update uploadFile.js

--- BackEnd/Utils/uploadFile.js
+++ BackEnd/Utils/uploadFile.js
@@ -1,10 +1,10 @@
 import multer from "multer";
 import path from "path";
 
-// Configure the storage location and file naming for uploaded files
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "Public/media");  // Ensure this directory exists
+    cb(null, "Public/media");  
   },
   filename: (req, file, cb) => {
     cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
