7f31fbe2737e59f652b512bc23ee3cad73c74c50
RasoulJf <rasouljalilian8@gmail.com>
2025-03-16 12:17:54 +0330

    Update ActivityMd.js

---
+++ BackEnd/Models/ActivityMd.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const activitySchema = new mongoose.Schema({
+  parent: {
+    type: String,
+    enum:['فعالیت‌های آموزشی','فعالیت‌های شغلی','فعالیت‌های داوطلبانه','فعالیت‌های توسعه فردی'],
+    required: true,
+  },
+  name: {
+    type:String,
+    required:[true,'Activity name is required']
+  },
+  description: String,
+});
+
+const Activity = mongoose.model('Activity',activitySchema)
+
+export default Activity
