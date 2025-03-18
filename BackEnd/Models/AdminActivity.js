165bd4636623745358e1ae00c8b1700240fc7df6
Frolic7 <a.alereza50@gmail.com>
2025-03-19 00:43:47 +0330

    AdminActivity

---
+++ BackEnd/Models/AdminActivity.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const adminActivitySchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,  
+    },
+    average:{
+        type:Number,
+    },
+    activityId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Activity",
+      required: true,
+      index: true, 
+    },
+    details: {
+      type: String,
+      required: [true, "Details is required"], 
+      trim: true, 
+    },
+    scoreAwarded: {
+      type: Number,      
+      default: 0,   
+    },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+const AdminActivity = mongoose.model(
+  "AdminActivity",
+  adminActivitySchema
+);
+
+export default AdminActivity;
