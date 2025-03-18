43ba619b4b9ea7adff95afb1fd07410251529c8b
RasoulJf <rasouljalilian8@gmail.com>
2025-03-18 13:00:58 +0330

    Update StudentActivity.js

---
+++ BackEnd/Models/StudentActivity.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const studentActivitySchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true, 
+    },
+    activityId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Activity",
+      required: true,
+      index: true, 
+    },
+    details: {
+      type: String,
+      required: [true, "Details is required"],
+      trim: true, 
+    },
+    status: {
+      type: String,
+      enum: ["pending", "approved", "rejected"],
+      default: "pending",
+    },
+    scoreAwarded: {
+      type: Number,
+      default: 0, 
+    },
+    adminComment: {
+      type: String,
+      trim: true,
+    },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+const StudentActivity = mongoose.model(
+  "StudentActivity",
+  studentActivitySchema
+);
+
+export default StudentActivity;
