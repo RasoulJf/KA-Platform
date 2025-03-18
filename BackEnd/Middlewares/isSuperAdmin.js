e01f93c8bd9f71069c604a6d199fcabb73909520
RasoulJf <rasouljalilian8@gmail.com>
2025-03-19 00:22:28 +0330

    Update isSuperAdmin.js

--- BackEnd/Middlewares/isSuperAdmin.js
+++ BackEnd/Middlewares/isSuperAdmin.js
@@ -2,7 +2,7 @@ import Jwt from "jsonwebtoken";
 import catchAsync from "../Utils/catchAsync.js";
 import HandleERROR from "../Utils/handleError.js";
 
-const isAdmin = catchAsync(async (req, res, next) => {
+const isAdmin = catchAsync(async (req, res, next) => { 
   const token = req.headers.authorization.split(" ")[1];
   const { id, role } = Jwt.verify(token, process.env.JWT_SECRET);
   req.userId = id;
