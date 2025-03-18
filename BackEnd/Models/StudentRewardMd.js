07de1e9d3c0260794fe2ed155e379b67a08b50fc
RasoulJf <rasouljalilian8@gmail.com>
2025-03-19 00:22:16 +0330

    Update StudentRewardMd.js

--- BackEnd/Models/StudentRewardMd.js
+++ BackEnd/Models/StudentRewardMd.js
@@ -6,7 +6,7 @@ const studentRewardSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
-      index: true,
+      index: true, 
     },
     rewardId: {
       type: mongoose.Schema.Types.ObjectId,
