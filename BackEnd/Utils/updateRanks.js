d2a7f8d17fabdabd6a0d30789eed15bde4881986
RasoulJf <rasouljalilian8@gmail.com>
2025-03-18 13:00:54 +0330

    Update updateRanks.js

--- BackEnd/Utils/updateRanks.js
+++ BackEnd/Utils/updateRanks.js
@@ -1,31 +1,52 @@
 import User from "../Models/UserMd.js";
 
-async function updateRanks() {
-    const students = await User.find({ role: 'Student' }).sort({ score: -1 });
+async function updateStudentRankings() {
+  const students = await User.find({ role: "Student" }).sort({ score: -1 });
 
-    students.forEach((student, index) => {
-        student.rankInSchool = index + 1;
+  const bulkUpdates = [];
+
+  students.forEach((student, index) => {
+    bulkUpdates.push({
+      updateOne: {
+        filter: { _id: student._id },
+        update: { rankInSchool: index + 1 },
+      },
     });
+  });
 
-    const grades = await User.distinct("grade");
-    for (const grade of grades) {
-        const studentsInGrade = await User.find({ role: 'Student', grade }).sort({ score: -1 });
-        studentsInGrade.forEach((student, index) => { 
-            student.rankInGrade = index + 1;
-        });
-        await Promise.all(studentsInGrade.map(student => student.save()));
-    }
+  const grades = [...new Set(students.map((student) => student.grade))];
+  for (const grade of grades) {
+    const studentsInGrade = students
+      .filter((student) => student.grade === grade)
+      .sort((a, b) => b.score - a.score);
+    studentsInGrade.forEach((student, index) => {
+      bulkUpdates.push({
+        updateOne: {
+          filter: { _id: student._id },
+          update: { rankInGrade: index + 1 },
+        },
+      });
+    });
+  }
 
-    const classes = await User.distinct("class");
-    for (const classId of classes) {
-        const studentsInClass = await User.find({ role: 'Student', class: classId }).sort({ score: -1 }); 
-        studentsInClass.forEach((student, index) => {
-            student.rankInClass = index + 1;
-        });
-        await Promise.all(studentsInClass.map(student => student.save()));
-    }
+  const classes = [...new Set(students.map((student) => student.class))];
+  for (const classId of classes) {
+    const studentsInClass = students
+      .filter((student) => student.class === classId)
+      .sort((a, b) => b.score - a.score);
+    studentsInClass.forEach((student, index) => {
+      bulkUpdates.push({
+        updateOne: {
+          filter: { _id: student._id },
+          update: { rankInClass: index + 1 },
+        },
+      });
+    });
+  }
 
-    await Promise.all(students.map(student => student.save())); 
+  if (bulkUpdates.length > 0) {
+    await User.bulkWrite(bulkUpdates);
+  }
 }
 
-export default updateRanks;
+export default updateStudentRankings;
