7fc964c65a7d5d8ee233082e0b8edd3007a6ef64
RasoulJf <rasouljalilian8@gmail.com>
2025-03-19 00:22:14 +0330

    Update UserMd.js

--- BackEnd/Models/UserMd.js
+++ BackEnd/Models/UserMd.js
@@ -13,7 +13,7 @@ const userSchema = new mongoose.Schema(
     },
     role: {
       type: String,
-      enum: ["student", "admin", "superAdmin"],
+      enum: ["student", "admin", "superAdmin"], 
       default: "Student",
     },
     idCode: {
