cf04baddec07d519c692a73298ba0837552f6bfd
Frolic7 <a.alereza50@gmail.com>
2025-03-19 03:36:43 +0330

    changeStatus

--- BackEnd/Controllers/StudentActivityCn.js
+++ BackEnd/Controllers/StudentActivityCn.js
@@ -1,9 +1,26 @@
 import StudentActivity from "../Models/StudentActivityMd.js";
 import catchAsync from "../Utils/catchAsync.js";
 import ApiFeatures from "../Utils/apiFeatures.js";
+import User from "../Models/UserMd.js";
 
-export const changeStatus=catchAsync(async(req,res,next)=>{
-
+export const changeStatus = catchAsync(async (req, res, next) => {
+    const { id } = req.params
+    const { status } = req.body
+    const studentActivity = await StudentActivity.findByIdAndUpdate(id, { status }, {
+        new: true,
+        runValidators: true
+    })
+    if (status == "approved") {
+        const {scoreAwarded}=req.body
+        const user = await User.findById(req.body.userId)
+            user.score=user.score+scoreAwarded
+        await user.save()
+        updateStudentRankings()
+    }
+    return res.status(200).json({
+        data: studentActivity,
+        success: true
+    })
 })
 export const createStudentActivity=catchAsync(async(req,res,next)=>{
     const studentActivity=await StudentActivity.create(req.body)
--- BackEnd/Controllers/StudentRewardCn.js
+++ BackEnd/Controllers/StudentRewardCn.js
@@ -1,12 +1,38 @@
 import StudentReward from "../Models/StudentRewardMd.js";
 import catchAsync from "../Utils/catchAsync.js";
 import ApiFeatures from "../Utils/apiFeatures.js";
+import User from "../Models/UserMd.js";
+import HandleERROR from "../Utils/handleError.js";
+import updateStudentRankings from "../Utils/updateRanks.js";
 
-export const createStudentReward=catchAsync(async(req,res,next)=>{
-    const studentReward=await StudentReward.create(req.body)
+export const changeStatus = catchAsync(async (req, res, next) => {
+    const { id } = req.params
+    const { status } = req.body
+    const studentReward = await StudentReward.findByIdAndUpdate(id, { status }, {
+        new: true,
+        runValidators: true
+    })
+    if (status == "approved") {
+        const {token}=req.body
+        const user = await User.findById(req.body.userId)
+        if((0.95 * user.score) >= token){
+            user.score=user.score-token
+        }else{
+            return next(new HandleERROR("user not enough score",400))
+        }
+        await user.save()
+        updateStudentRankings()
+    }
+    return res.status(200).json({
+        data: studentReward,
+        success: true
+    })
+})
+export const createStudentReward = catchAsync(async (req, res, next) => {
+    const studentReward = await StudentReward.create(req.body)
     return res.status(201).json({
-        success:true,
+        success: true,
         message: "studentReward created successfully",
-        data:studentReward
+        data: studentReward
     })
 })
