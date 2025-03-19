6cc1928423066b3c943f66c1509adf42397f2bd1
RasoulJf <rasouljalilian8@gmail.com>
2025-03-19 22:52:22 +0330

    Update AuthCn.js

--- BackEnd/Controllers/AuthCn.js
+++ BackEnd/Controllers/AuthCn.js
@@ -4,37 +4,44 @@ import bcryptjs from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import HandleERROR from "../Utils/handleError.js";
 export const register=catchAsync(async(req,res,next)=>{
-    const {idCode='',role="",...others}=req.body
-    const hashPass=bcryptjs.hashSync(idCode,10)
-    await User.create({...others,idCode:hashPass})
+    const {idCode='',password='',role=null, ...others}=req.body
+    let pass 
+     if(role === 'student'){
+        pass = `s${idCode}`
+    }else {
+        pass = `a${idCode}`
+    }
+    const hashPass=bcryptjs.hashSync(pass,10)
+    await User.create({...others,idCode:idCode,password:hashPass,role:role})
     return res.status(201).json({
         message:'register successfully',
         success:true
     })
 })
 export const login=catchAsync(async(req,res,next)=>{
-    const {fullName=null,idCode=null}=req.body
-    if(!fullName || !idCode){
-        return next(new HandleERROR('fullName and idCode is required'))
+    const {idCode=null,password=null}=req.body
+    if(!password || !idCode){
+        return next(new HandleERROR('idCode and password is required'))
     }
-    const user=await User.findOne({fullName})
+    const user=await User.findOne({idCode})
     if(!user){
         return next(new HandleERROR('user not found',404))
     }
-    const checkidCode=bcryptjs.compareSync(idCode,user.idCode)
-    if(!checkidCode){
-        return next(new HandleERROR('fullName or idCode is incorrect',400))
+    const checkPassword=bcryptjs.compareSync(password,user?.password)
+    if(!checkPassword){
+        return next(new HandleERROR('password or idCode is incorrect',400))
     }
-    const token=jwt.sign({id:user._id,role:user.role},process.env.JWT_SECRET)
+    const token=jwt.sign({id:user?._id,role:user?.role},process.env.JWT_SECRET)
     return res.status(200).json({
         message:"login successfully",
         success:true,
         data:{
             token,
             user:{
-                id:user._id,
-                fullName:user.fullName,
-                role:user.role
+                id:user?._id,
+                fullName:user?.fullName,
+                role:user?.role,
+                idcode:user?.idCode
             }
         }
     })
