4c41388d730c0a69292cff1c7229110252a2d76b
RasoulJf <rasouljalilian8@gmail.com>
2025-03-25 15:56:18 +0330

    Create StudentAvreage.js

---
+++ BackEnd/Models/StudentAvreage.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const studentAvreageSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+    avreage: {
+      type: Number,
+    },
+    scoreAwarded: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+const StudentAvreage = mongoose.model("StudentAvreage", studentAvreageSchema);
+
+export default StudentAvreage;
