44a38e85c632e4fdc64de8505a49e7865f34b3fe
Amirali Alereza <a.alereza50@gmail.com>
2026-05-29 19:10:18 +0330

    dynamucicon

---
+++ FrontEnd/Admin/src/Components/DynamicIcon.jsx
@@ -0,0 +1,23 @@
+// src/Components/DynamicIcon.jsx
+import * as Iconsax from 'iconsax-react';
+
+const DynamicIcon = ({ name, size = 24, variant = "Outline", color, ...props }) => {
+  if (!name) {
+    const FallbackIcon = Iconsax.Gift;
+    return <FallbackIcon size={size} variant={variant} {...props} />;
+  }
+
+  // Normalize name to PascalCase: "gift" → "Gift", "arrow-right" → "ArrowRight"
+//   const normalizedName = name
+//     .split(/[-_\s]+/)
+//     .map(part => part.charAt(0).toUpperCase() + part.slice(1).toLowerCase())
+//     .join('');
+
+  const IconComponent = Iconsax[name] || Iconsax.Gift;
+  console.log(IconComponent)
+
+  return <IconComponent size={size} color={color ? color : "#d9e3f0"}
+  variant={variant} {...props} />;
+};
+
+export default DynamicIcon;
